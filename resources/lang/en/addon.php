a843ec4eb54b4996ced42c417ecdecbef9560155
William <william@pixney.com>
2018-12-07 22:32:54 +0100

    first commit

---
+++ resources/lang/en/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => 'Wysiwyg Image Block',
+    'name'        => 'Wysiwyg Image Block Extension',
+    'description' => ''
+];
---
+++ src/WysiwygImageBlockExtension.php
@@ -0,0 +1,21 @@
+<?php namespace Pixney\WysiwygImageBlockExtension;
+
+use Anomaly\Streams\Platform\Addon\Extension\Extension;
+
+class WysiwygImageBlockExtension extends Extension
+{
+
+    /**
+     * This extension provides...
+     *
+     * This should contain the dot namespace
+     * of the addon this extension is for followed
+     * by the purpose.variation of the extension.
+     *
+     * For example anomaly.module.store::gateway.stripe
+     *
+     * @var null|string
+     */
+    protected $provides = null;
+
+}
