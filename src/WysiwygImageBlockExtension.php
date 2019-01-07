3a3c44e892061bb0d596428e2fbba86147294f59
William <william@pixney.com>
2019-01-07 01:10:52 +0100

    Bugfix

--- src/WysiwygImageBlockExtension.php
+++ src/WysiwygImageBlockExtension.php
@@ -4,21 +4,18 @@ namespace Pixney\WysiwygImageBlockExtension;
 
 use Anomaly\BlocksModule\Block\BlockExtension;
 use Pixney\WysiwygImageBlockExtension\Block\BlockModel;
+use Anomaly\SettingsModule\Setting\Contract\SettingRepositoryInterface;
 
 class WysiwygImageBlockExtension extends BlockExtension
 {
     protected $provides = 'anomaly.module.blocks::block.wysiwyg_image';
     protected $model    = BlockModel::class;
-    protected $wrapper  = 'theme::blocks/global/wrapper';
 
     public function getView()
     {
-        // TODO : Either get active theme or config..
-        return 'theme::blocks/wysiwyg_image';
-
-        // if ($this->block->configuration('something_special')) {
-        //     return 'my_company.extension.awesome_block::alternate_view';
-        // }
-        // return $this->view;
+        $settings      = app(SettingRepositoryInterface::class);
+        $setting       = $settings->get('streams::standard_theme');
+        $this->wrapper = $setting->value . '::blocks/global/wrapper';
+        return $setting->value . '::blocks/wysiwyg_image';
     }
 }
